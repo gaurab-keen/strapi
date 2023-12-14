e7a7bfc522e20bd255e04a7b9c8f7cfddc62e91a
imFarhan02 <mohammad.farhan@fosteringlinux.com>
2023-12-14 16:15:28 +0530

    Local strapi code pushed

---
+++ src/api/test5/routes/test5.js
@@ -0,0 +1,13 @@
+module.exports = {
+  routes: [
+    {
+     method: 'GET',
+     path: '/getMinistryData',
+     handler: 'test6.testData6',
+     config: {
+       policies: [],
+       middlewares: [],
+     },
+    },
+  ],
+};
